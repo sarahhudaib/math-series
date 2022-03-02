1907981cd64a12890559d5d5526a66150b664ff9
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-02 11:30:25 +0300

    add 100 %

--- math_series/series.py
+++ math_series/series.py
@@ -1,18 +1,17 @@
-
 def fibonacci(n):
     if (n == 0):
-        return []
-    elif n == 1:
         return [0]
+    elif n == 1:
+        return [1]
     elif n == 2:
         return [0,1]
     elif (n < 0):
         return []
     else:
-        x = fibonacci(n-1)
+        y = fibonacci(n-1)
         # the new element the sum of the last two elements
-        x.append(sum(x[:-3:-1]))
-        return x
+        y.append(sum(y[:-3:-1]))
+        return y
 '''
     This function should have one parameter n. 
     & should return the nth value in the fibonacci series. 
@@ -20,21 +19,31 @@ def fibonacci(n):
     '''
 
 def lucas(n):
-    if n==0:
+    # return fibonacci(n, x=2, z=1)
+    if n == 0:
         return 2
-    if n==1:
-        return 1
-    return lucas(n-1) + lucas(n-2)
+    elif n == 1:
+        return n
+    elif n > 0:
+        return lucas(n-1) + lucas(n-2)
+    elif n < 0:
+        return lucas(n+2) - lucas(n+1)
+    
 
 '''
 The function lucas should return the nth value in the lucas numbers 
 
     '''
-   
-
-
-def sum_series(n,x=0,z=1):
-    '''
+    
+def sum_series(n, x=0, z=1):
+    #Determine nth number in series
+    if n == 0:
+        return x
+    elif n == 1:
+        return z
+    else:
+        return sum_series(n-1,x,z)+ sum_series(n-2,x,z)
+'''
 The required parameter will determine which element in the series to print. 
 The two optional parameters will have default values of 0 and 1 and will determine 
 the first two values for the series to be produced.
@@ -43,3 +52,7 @@ Calling it with the optional arguments 2 and 1 will produce values from the luca
 Other values for the optional parameters will produce other series.
     '''
     
+    
+# print(fibonacci(10))
+# print(lucas(9))
+print(sum_series(7,7,9))
--- tests/test_series.py
+++ tests/test_series.py
@@ -4,12 +4,14 @@ from math_series.series import *
 def test_version():
     assert __version__ == '0.1.0'
 
+###################### fibonacci #########################   
+
 def test_fibonacci_0():
-    assert(fibonacci(0) == [])
+    assert(fibonacci(0) == [0])
 
 def test_one():
     actual = fibonacci(1)
-    expected = [0]
+    expected = [1]
     assert actual == expected
     
 def test_two():
@@ -25,3 +27,56 @@ def test_fibo_3():
     
 def test_fibo_7():
     assert (fibonacci(7) == [0, 1, 1, 2, 3, 5, 8])
+    
+###################### LUCAS #########################
+    
+def test_zero():
+    expected = 2
+    actual = lucas(0)
+    assert actual == expected
+
+def test_one():
+    expected = 1
+    actual = lucas(1)
+    assert actual == expected
+
+def test_seven():
+    expected = 29
+    actual = lucas(7)
+    assert actual == expected
+
+def test_eleven():
+    expected = 199
+    actual = lucas(11)
+    assert actual == expected
+
+def test_fifteen():
+    expected = 1364
+    actual = lucas(15)
+    assert actual == expected
+    
+###################### sum_series #########################    
+def test_two():
+    expected = 1
+    actual = sum_series(2)
+    assert actual == expected
+
+def test_three():
+    expected = 2
+    actual = sum_series(3)
+    assert actual == expected
+
+def test_four():
+    expected = 3
+    actual = sum_series(4)
+    assert actual == expected
+
+def test_three_parameters():
+    expected = 123
+    actual = sum_series(10,2,1)
+    assert actual == expected
+
+def test_three_parameterssss():
+    expected = 55
+    actual = sum_series(10,0,1)
+    assert actual == expected
